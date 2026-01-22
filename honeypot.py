0ada25a238b69ab9e61558d2805b365e8de827c5
l1ght-man <dahlamarzouki@gmail.com>
2026-01-22 14:13:37 +0100

    Add fake website HTML and enhance honeypot logging for HTTP requests

--- honeypot.py
+++ honeypot.py
@@ -12,11 +12,17 @@ LOG_DIR.mkdir(exist_ok=True)
 class Honeypot:
     def __init__(self, ports=[22,21,80]):
         self.ports = ports
+        with open ('fake_website.html' , 'rb') as f:
+            html_content = f.read()
         self.banners = {
-            22: b"SSH-2.0-FakeSSH\r\n",
-            21: b"220 Fake FTP Server ready\r\n",
-            80: b"HTTP/1.1 200 OK\r\n\n<h1>Honeypot Page</h1>\r\n"
-        }
+    22: b"SSH-2.0-FakeSSH\r\n",
+    21: b"220 Fake FTP Server ready\r\n",
+    80: b"""HTTP/1.1 200 OK\r\n"""
+        b"""Server: Apache/2.4.41 (Ubuntu)\r\n"""
+        b"""Content-Type: text/html; charset=UTF-8\r\n"""
+        b"""Connection: close\r\n\r\n""" + html_content
+}
+ 
     def log_activity(self, ip , port , data=""):
         timestamp = datetime.datetime.now().isoformat()
         activity = {
@@ -35,8 +41,27 @@ class Honeypot:
             banner = self.banners.get(port, b"Unknown service\r\n")
             client_sock.send(banner)
             self.log_activity(ip,port, "banner sent")
-            data= client_sock.recv(1024).decode('utf-8', errors='ignore')
-            self.log_activity(ip,port,f"Received: {data}")
+            if port == 80:
+                        raw_data = client_sock.recv(1024)
+                        http_request = raw_data.decode('utf-8', errors='ignore')
+                        lines = http_request.split('\r\n')
+                        if lines:
+                            first_line = lines[0]
+                            method, path, protocol = first_line.split(' ', 2)
+                            self.log_activity(ip, port, f"HTTP {method} {path}")
+
+                            headers = {}
+                            for line in lines[1:]:
+                                if ': ' in line:
+                                    key, value = line.split(': ', 1)
+                                    headers[key] = value.strip()
+                            if 'User-Agent' in headers:
+                                self.log_activity(ip, port, f"User-Agent: {headers['User-Agent']}")
+                            else:
+                                self.log_activity(ip, port, "Empty HTTP request")
+            else:
+                data= client_sock.recv(1024).decode('utf-8', errors='ignore')
+                self.log_activity(ip,port,f"Received: {data}")
         except Exception as e:
             self.log_activity(ip, port, f"Error : {str(e)}")
         finally:
